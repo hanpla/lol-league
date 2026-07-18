0d3224ce214ac86f4c61f87d134b56a48a20707c
Hanpla <hanpla25@gmail.com>
2026-07-18 11:55:52 +0900

    chore: title 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "롤 리그",
+  title: "롤 리그 대회 일정",
   description: "리그 오브 레전드의 대회 일정을 모아놓은 사이트 입니다.",
   verification: {
     google: "XdNWEY6QgvSgWWTcXJVBmzQnbxnn-5lc5mwJqOFPDJs",
